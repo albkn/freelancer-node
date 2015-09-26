1712351c6b7462b61296c55c84cca03b671e154a
Albert Kurniawan <kurniawan.albert19@gmail.com>
2015-09-26 18:29:54 +1000

    pls fix

--- freelancer.js
+++ freelancer.js
@@ -1,25 +1,23 @@
-Freelancer = {};
-
-// API constants
-// Freelancer.DEFAULT_HOST = 'freelancer.com';
-Freelancer.DEFAULT_HOST = 'hackathon.syd1.fln-dev.net';
-Freelancer.DEFAULT_PORT = '443';
-Freelancer.DEFAULT_PATH = '/api/';
-// Auth constants
-Freelancer.DEVELOPER_ID = '';
-Freelancer.DEVELOPER_KEY = '';
-
-// Import methods
-var RB = require('./lib/_resource-builder.js')(Freelancer);
-Freelancer.Projects = require('./lib/projects.js')(RB);
-Freelancer.Bids = require('./lib/bids.js')(RB);
-Freelancer.Jobs = require('./lib/jobs.js')(RB);
-Freelancer.Milestones = require('./lib/milestones.js')(RB);
-Freelancer.MilestoneRequests = require('./lib/milestone_requests.js')(RB);
-Freelancer.Reviews = require('./lib/reviews.js')(RB);
-
 module.exports = function(developerId, developerKey) {
+
+  Freelancer = {};
+  // Auth constants
   Freelancer.DEVELOPER_ID = developerId;
   Freelancer.DEVELOPER_KEY = developerKey;
+  // API constants
+  // Freelancer.DEFAULT_HOST = 'freelancer.com';
+  Freelancer.DEFAULT_HOST = 'hackathon.syd1.fln-dev.net';
+  Freelancer.DEFAULT_PORT = '443';
+  Freelancer.DEFAULT_PATH = '/api/';
+
+  // Import methods
+  var RB = require('./lib/_resource-builder.js')(Freelancer);
+  Freelancer.Projects = require('./lib/projects.js')(RB);
+  Freelancer.Bids = require('./lib/bids.js')(RB);
+  Freelancer.Jobs = require('./lib/jobs.js')(RB);
+  Freelancer.Milestones = require('./lib/milestones.js')(RB);
+  Freelancer.MilestoneRequests = require('./lib/milestone_requests.js')(RB);
+  Freelancer.Reviews = require('./lib/reviews.js')(RB);
+
   return Freelancer;
 };
